03f21193a7dc6df3a02372b7a12d2b2607470104
ikuokuo <ikuokuo@hotmail.com>
2021-07-09 11:11:42 +0800

    feat(web): scss

--- my-web/src/App.tsx
+++ my-web/src/App.tsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { Button } from 'antd';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 class App extends Component {
   render() {
